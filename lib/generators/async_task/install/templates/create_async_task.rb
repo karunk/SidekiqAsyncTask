670d961941386b9146205d285f94033d4eb72fe3
Karun <karunk@live.com>
2019-01-29 18:01:38 +0530

    add schema for AsyncTask

--- lib/generators/async_task/install/templates/create_async_task.rb
+++ lib/generators/async_task/install/templates/create_async_task.rb
@@ -1,5 +1,16 @@
 class CreateAsyncTask < ActiveRecord::Migration
   def change
-    #...
+    create_table :async_tasks do |t|
+      t.string :task_name, null: false
+      t.string :job_id
+      t.integer :perform_after
+      t.json :payload
+      t.datetime :started_at
+      t.datetime :completed_at
+      t.string :state, default: :scheduled, null: false
+      t.string :external_hash
+      t.integer :retry_count, default: 0
+      t.timestamps
+    end
   end
 end
