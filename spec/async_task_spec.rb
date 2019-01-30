4badbdc61010027c1910aa88529d2ee934e4aecc
Karun <karunk@live.com>
2019-01-30 18:44:06 +0530

    Add tests for AsyncTask::AsyncTask model attribute presence

--- spec/async_task_spec.rb
+++ spec/async_task_spec.rb
@@ -5,7 +5,17 @@ describe AsyncTask do
     expect(AsyncTask::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(false)
+  it 'AsyncTask model has required attributes' do
+    expect(AsyncTask::AsyncTask.new.attributes).to include('task_name')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('job_id')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('perform_after')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('payload')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('started_at')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('completed_at')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('state')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('started_at')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('external_hash')
+    expect(AsyncTask::AsyncTask.new.attributes).to include('retry_count')
+    expect(AsyncTask::AsyncTask.new.state).to eq('scheduled')
   end
 end
